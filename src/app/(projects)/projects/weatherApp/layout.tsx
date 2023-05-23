7f5e1965e5d939e050f24ae9108c638c093d5ad6
Joshua Levine <joshuaalevine3@gmail.com>
2023-05-23 02:13:27 -0400

    create custom header for weather app

--- src/app/(projects)/projects/weatherApp/layout.tsx
+++ src/app/(projects)/projects/weatherApp/layout.tsx
@@ -1,6 +1,7 @@
 import '../../../globals.css';
 import Body from '@/components/weatherAppComponents/Body';
 import Main from '@/components/weatherAppComponents/Main';
+import Header from '@/components/weatherAppComponents/header/Header';
 import React from 'react';
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <Body>
+        <Header />
         <Main>{children}</Main>
       </Body>
     </html>
---
+++ src/components/weatherAppComponents/header/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+import HeaderLink from './HeaderLink';
+
+export default function Header() {
+  return (
+    <header className='fixed w-full py-2 mx-auto backdrop-blur-md'>
+      <nav className='flex max-w-3xl px-5 mx-auto'>
+        <div>
+          <Link href={'/projects/weatherApp'}>
+            <h1 className='text-xl'>Weather App</h1>
+          </Link>
+        </div>
+        <div className='flex-1' />
+        <div>
+          <HeaderLink href={'/'}>Home</HeaderLink>
+        </div>
+      </nav>
+    </header>
+  );
+}
